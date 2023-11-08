286fc92eca564045222820736fe9ebea8aa7fc16
BuiQuangGiau1 <114122977+BuiQuangGiau1@users.noreply.github.com>
2023-11-08 22:10:46 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Piano from "./Piano";
+
+const App = () => {
+    return (
+        <div>
+            <Piano></Piano>
+        </div>
+    );
+};
+
+export default App;
